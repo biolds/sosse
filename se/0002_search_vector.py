0298798591c481aefc97a7c5ddf13dacd4ad11a9
Laurent DEFERT <laurent_defert@yahoo.fr>
2022-03-18 08:41:16 +0100

    use links text when building ranking

--- se/0002_search_vector.py
+++ se/0002_search_vector.py
@@ -9,7 +9,9 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(
             sql='''
-              CREATE FUNCTION weight_vector() RETURNS trigger AS $$
+              -- Doc update
+
+              CREATE FUNCTION doc_weight_vector() RETURNS trigger AS $$
               BEGIN
                 new.vector = setweight(to_tsvector(new.vector_lang, new.normalized_title), 'A') ||
                              setweight(to_tsvector(new.vector_lang, new.normalized_url), 'A') ||
@@ -22,7 +24,27 @@ class Migration(migrations.Migration):
               CREATE TRIGGER vector_column_trigger
               BEFORE INSERT OR UPDATE OF normalized_title, normalized_content, normalized_url, vector_lang
               ON se_document
-              FOR EACH ROW EXECUTE PROCEDURE weight_vector();
+              FOR EACH ROW EXECUTE PROCEDURE doc_weight_vector();
+
+              -- Link update
+
+              CREATE FUNCTION link_weight_vector() RETURNS trigger AS $$
+              BEGIN
+                UPDATE se_document SET
+                    vector = setweight(to_tsvector(vector_lang, se_document.normalized_title), 'A') ||
+                             setweight(to_tsvector(vector_lang, se_document.normalized_url), 'A') ||
+                             setweight(to_tsvector(vector_lang, (SELECT STRING_AGG(se_link.text, ' ') FROM se_link WHERE se_link.doc_to_id=se_document.id)), 'B') ||
+                             setweight(to_tsvector(vector_lang, se_document.normalized_content), 'C')
+                WHERE id = new.doc_to_id;
+                RETURN new;
+              END
+              $$ LANGUAGE plpgsql;
+
+              CREATE TRIGGER link_row_trigger
+              BEFORE INSERT OR UPDATE
+              ON se_link
+              FOR EACH ROW EXECUTE PROCEDURE link_weight_vector();
+
             ''',
 
             reverse_sql = '''

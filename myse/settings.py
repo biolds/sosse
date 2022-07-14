da28af1c1d513201a8bca7e0caacb372c4bca576
Laurent DEFERT <laurent_defert@yahoo.fr>
2022-07-14 16:14:09 -0400

    admin initial styling

--- myse/settings.py
+++ myse/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'se'
+    'se',
+    'django.contrib.admin',
 ]
 
 MIDDLEWARE = [

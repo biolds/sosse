0c843759fc0bf0c0b01f347f10d689a196b08013
Laurent DEFERT <laurent_defert@yahoo.fr>
2022-08-13 10:53:31 +0200

    css wip

---
+++ se/static/se/base.js
@@ -0,0 +1,47 @@
+function getLang() {
+  if (localStorage.getItem('myseLanguage')) {
+    return localStorage.getItem('myseLanguage');
+  } else {
+    return (navigator.language || navigator.userLanguage).replace(/-.*/, '');
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    const buttons = document.getElementsByClassName('menu_button');
+    for (let i = 0; i < buttons.length; i++) {
+        const el = buttons[i];
+        el.addEventListener('click', function(ev) {
+            const panel = ev.target.parentElement.getElementsByClassName('panel')[0];
+            const panelDisplayed = panel.style.display === 'block';
+            panel.style.display = panelDisplayed ? 'none': 'block';
+        }, false);
+    }
+});
+
+// Close menu panels on click outside
+// https://www.w3docs.com/snippets/javascript/how-to-detect-a-click-outside-an-element.html
+document.addEventListener("click", (evt) => {
+    const panels = document.getElementsByClassName('panel');
+    for (let i = 0; i < panels.length; i++) {
+        const panel = panels[i];
+
+        if (panel.style.display === 'none') {
+            continue;
+        }
+
+        let targetEl = evt.target;
+        let needHide = true;
+        const button = panel.parentElement.getElementsByClassName('menu_button')[0];
+
+        while (targetEl) {
+            if(targetEl === button) {
+                needHide = false;
+                break
+            }
+            targetEl = targetEl.parentNode;
+        }
+        if (needHide) {
+            panel.style.display = 'none';
+        }
+    }
+});

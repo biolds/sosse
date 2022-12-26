e0f5bce048a7b0d5ff5acd9c042831ead196f2ec
Laurent DEFERT <laurent_defert@yahoo.fr>
2022-12-26 19:09:39 +0100

    add copyright header

--- sosse/settings.py
+++ sosse/settings.py
@@ -1,3 +1,18 @@
+# Copyright 2022-2023 Laurent Defert
+#
+#  This file is part of SOSSE.
+#
+# SOSSE is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# SOSSE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with SOSSE.
+# If not, see <https://www.gnu.org/licenses/>.
+
 """
 Django settings for SOSSE project.
 

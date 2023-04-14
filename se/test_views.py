5956cc4d7bfe8d110a2788edaf542781c80543b5
Laurent Defert <laurent_defert@yahoo.fr>
2023-04-14 10:14:39 +0000

    improve views coverage

--- se/test_views.py
+++ se/test_views.py
@@ -13,12 +13,15 @@
 # You should have received a copy of the GNU Affero General Public License along with SOSSE.
 # If not, see <https://www.gnu.org/licenses/>.
 
+from urllib.parse import quote
+
 from django.contrib.auth.models import User
 from django.test import RequestFactory, TestCase
 from django.test.client import Client
 from django.utils import timezone
 
 from se.atom import atom
+from se.cached import cache_redirect
 from se.models import CrawlerStats, CrawlPolicy, Document, DomainSetting
 from se.screenshot import screenshot
 from se.stats import stats
@@ -27,7 +30,7 @@ from se.words import words
 from se.www import www
 
 
-CRAWL_URL = 'http://127.0.0.1:8000/'
+CRAWL_URL = 'http://127.0.0.1:8000/cookies'
 
 
 class ViewsTest(TestCase):
@@ -56,6 +59,14 @@ class ViewsTest(TestCase):
         self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
         self.assertTrue(self.client.login(username='admin', password='admin'))
 
+    def _request_from_factory(self, url):
+        request = self.factory.get(url)
+        request.META['REQUEST_URI'] = url
+        request.META['REQUEST_SCHEME'] = 'http'
+        request.META['HTTP_HOST'] = '127.0.0.1'
+        request.user = self.user
+        return request
+
     def test_views(self):
         for (url, view) in (('/?q=page', search),
                             ('/about/', about),
@@ -69,26 +80,32 @@ class ViewsTest(TestCase):
                             ('/word_stats/?q=page', word_stats),
                             ('/opensearch.xml', opensearch),
                             ('/www/' + CRAWL_URL, www),
+                            ('/www/http://unknown/', www),
                             ('/words/' + CRAWL_URL, words),
                             ('/screenshot/' + CRAWL_URL, screenshot)):
-            request = self.factory.get(url)
-            request.META['REQUEST_URI'] = url
-            request.META['REQUEST_SCHEME'] = 'http'
-            request.META['HTTP_HOST'] = '127.0.0.1'
-            request.user = self.user
+
+            request = self._request_from_factory(url)
             try:
                 response = view(request)
             except:  # noqa
                 raise Exception('Failed on %s' % url)
             self.assertEqual(response.status_code, 200, url)
 
+    def test_cache_redirect(self):
+        request = self._request_from_factory('/cache/' + CRAWL_URL)
+        response = cache_redirect(request)
+        self.assertEqual(response.status_code, 302, response)
+        self.assertEqual(response.url, '/screenshot/' + CRAWL_URL, response)
+
     def test_admin_views(self):
-        for url in ('/admin/', '/admin/se/document/queue/', '/admin/se/document/crawl_status/',
+        for url in ('/admin/', '/admin/se/document/queue/', '/admin/se/document/crawl_status/', '/admin/se/document/crawl_status_content/',
                     '/admin/se/crawlpolicy/', '/admin/se/crawlpolicy/%s/change/' % self.crawl_policy.id,
                     '/admin/se/document/', '/admin/se/document/?is_queued=yes', '/admin/se/document/?is_queued=no',
                     '/admin/se/document/?has_error=yes', '/admin/se/document/?has_error=no',
                     '/admin/se/document/%s/change/' % self.doc.id,
-                    '/admin/se/domainsetting/', '/admin/se/cookie/', '/admin/se/excludedurl/', '/admin/se/searchengine/'):
+                    '/admin/se/domainsetting/', '/admin/se/domainsetting/%s/change/' % DomainSetting.get_from_url(CRAWL_URL).id,
+                    '/admin/se/cookie/', '/admin/se/cookie/?q=%s' % quote(CRAWL_URL),
+                    '/admin/se/excludedurl/', '/admin/se/searchengine/'):
             response = self.client.get(url)
             self.assertEqual(response.status_code, 200, '%s / %s' % (url, response))
 
@@ -102,7 +119,12 @@ class ViewsTest(TestCase):
         self.assertEqual(response.status_code, 302, '%s / %s' % (action, response))
         self.assertEqual(response.url, '/admin/se/document/crawl_status/', '%s / %s' % (action, response))
 
-    def test_admin_crawl(self):
+    def test_admin_crawl_status_actions(self):
+        for action in ('pause', 'resume'):
+            response = self.client.post('/admin/se/document/crawl_status/', {action: "1"})
+            self.assertEqual(response.status_code, 200, '%s / %s' % (action, response))
+
+    def test_admin_add_crawl(self):
         response = self.client.post('/admin/se/document/queue_confirm/', {'url': CRAWL_URL})
         self.assertEqual(response.status_code, 200, response)
 

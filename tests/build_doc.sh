6b7e2f15e34f38821ccb4f57b368ed04b6e68574
Laurent Defert <laurent_defert@yahoo.fr>
2023-04-29 08:16:02 -0400

    doc building helper script

---
+++ tests/build_doc.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+cd "$(dirname "$0")/.."
+rm -rf doc/build
+./sosse-admin extract_doc cli > doc/source/cli_generated.rst
+./sosse-admin extract_doc conf > doc/source/config_file_generated.rst
+make -C doc html 

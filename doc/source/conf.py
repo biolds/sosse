c3e3bc950e851f874f0e8a19e265afc8e43a95de
Laurent Defert <laurent_defert@yahoo.fr>
2023-04-29 20:08:19 -0400

    use furo's theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
 html_logo = '../../se/static/se/logo.svg'
 html_favicon = '../../se/static/se/logo.svg'

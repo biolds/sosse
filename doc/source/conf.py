9719224b2ada3cfece87647fbc5f5d512fda873f
Laurent Defert <laurent_defert@yahoo.fr>
2023-11-11 11:29:51 +0100

    v1.7.0 release

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,7 +26,7 @@ import sys
 project = 'SOSSE'
 copyright = '2023, Laurent Defert'
 author = 'Laurent Defert'
-release = '1.6'
+release = '1.7'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration

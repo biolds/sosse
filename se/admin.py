bbd44ccd7f7839ad6d5daccad484c19dde828bbf
Laurent Defert <laurent_defert@yahoo.fr>
2023-12-09 18:25:49 +0100

    prevent enabling HTML snapshots when browser crawling is not set explicitly

--- se/admin.py
+++ se/admin.py
@@ -505,12 +505,15 @@ class CrawlPolicyForm(forms.ModelForm):
             if cleaned_data['create_thumbnails']:
                 self.add_error('default_browse_mode', 'Browsing mode must be set to Chromium or Firefox to create thumbnails')
                 self.add_error('create_thumbnails', 'Browsing mode must be set to Chromium or Firefox to create thumbnails')
-            elif cleaned_data['take_screenshots']:
+            if cleaned_data['take_screenshots']:
                 self.add_error('default_browse_mode', 'Browsing mode must be set to Chromium or Firefox to take screenshots')
                 self.add_error('take_screenshots', 'Browsing mode must be set to Chromium or Firefox to take screenshots')
-            elif cleaned_data['script']:
+            if cleaned_data['script']:
                 self.add_error('default_browse_mode', 'Browsing mode must be set to Chromium or Firefox to run a script')
                 self.add_error('script', 'Browsing mode must be set to Chromium or Firefox to run a script')
+            if cleaned_data['snapshot_html']:
+                self.add_error('default_browse_mode', 'Browsing mode must be set to Chromium or Firefox to take HTML snapshots')
+                self.add_error('snapshot_html', 'Browsing mode must be set to Chromium or Firefox to take HTML snapshots')
         return cleaned_data
 
 

68bd1c66f7979bba5be949888263823428c29bda
Laurent Defert <laurent_defert@yahoo.fr>
2025-05-22 23:24:17 +0200

    make tests more robust when runing locally

--- tests/robotframework/start.sh
+++ tests/robotframework/start.sh
@@ -5,5 +5,10 @@ su - postgres -c "psql -d sosse --command=\"DELETE FROM se_document;\""
 su - postgres -c "psql -d sosse --command=\"DELETE FROM se_cookie;\""
 su - postgres -c "psql -d sosse --command=\"DELETE FROM se_domainsetting;\""
 su - postgres -c "psql -d sosse --command=\"DELETE FROM se_searchhistory;\""
-exec /robotframework-venv/bin/robot --exitonerror --exitonfailure tests
+su - postgres -c "psql -d sosse --command=\"TRUNCATE auth_user CASCADE;\""
+su - postgres -c "psql -d sosse --command=\"ALTER SEQUENCE auth_user_id_seq RESTART WITH 1;\""
+su - postgres -c "psql -d sosse --command=\"TRUNCATE se_webhook CASCADE;\""
+sosse-admin update_se
+sosse-admin default_admin
+exec /opt/venv-robotframework/bin/robot -V config.yaml --exitonerror --exitonfailure docs/
 #exec /robotframework-venv/bin/robot --exitonerror --exitonfailure 02_*.robot

1fa69c52d506ce430b4ff06a10373eb2fd530829
Laurent Defert <laurent_defert@yahoo.fr>
2025-06-02 15:41:26 +0200

    remove dangerous cleaning commands

--- tests/robotframework/start.sh
+++ tests/robotframework/start.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/bash
-su - postgres -c "psql -d sosse --command=\"DELETE FROM se_crawlpolicy;\""
-su - postgres -c "psql -d sosse --command=\"DELETE FROM se_link;\""
-su - postgres -c "psql -d sosse --command=\"DELETE FROM se_document;\""
-su - postgres -c "psql -d sosse --command=\"DELETE FROM se_cookie;\""
-su - postgres -c "psql -d sosse --command=\"DELETE FROM se_domainsetting;\""
-su - postgres -c "psql -d sosse --command=\"DELETE FROM se_searchhistory;\""
-su - postgres -c "psql -d sosse --command=\"TRUNCATE auth_user CASCADE;\""
-su - postgres -c "psql -d sosse --command=\"ALTER SEQUENCE auth_user_id_seq RESTART WITH 1;\""
-su - postgres -c "psql -d sosse --command=\"TRUNCATE se_webhook CASCADE;\""
 sosse-admin update_se
 sosse-admin default_admin
 exec /opt/venv-robotframework/bin/robot -V config.yaml --exitonerror --exitonfailure guides/
